1a3296383b51c65bfe389958fb0faeaf00282db4
Esteban <lesteban.ramos.olort@gmail.com>
2023-09-14 19:58:40 -0500

    Initial commit

---
+++ src/main/java/com/happyfeet/HappyFeetBackendApplication.java
@@ -0,0 +1,13 @@
+package com.happyfeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyFeetBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyFeetBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/happyfeet/HappyFeetBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.happyfeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyFeetBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
